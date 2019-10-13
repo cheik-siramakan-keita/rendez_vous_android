3e095e6a9b1a405420457b40a43c5faa2335d4f4
Cheik-Siramakan Keita <cheiksiramakankeita@gmail.com>
2019-10-14 00:31:43 +0200

    J'ai modifié le nom de l'application affiché lorsque la personne ouvre l'application.

--- app/src/main/java/com/example/rendez_vous_android/MainActivity.java
+++ app/src/main/java/com/example/rendez_vous_android/MainActivity.java
@@ -1,4 +1,25 @@
 package com.example.rendez_vous_android;
 
-public class MainActivity {
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webview;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webview =(WebView)findViewById(R.id.webView);
+
+        webview.setWebViewClient(new WebViewClient());
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webview.loadUrl("http://cheikkeita.ddns.net:8005/accueil");
+
+    }
 }
